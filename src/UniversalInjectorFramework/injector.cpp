b6901a7a7da26cb601228bd02a50914b78c73119
AtomCrafty <atomcrafty@frucost.net>
2022-03-01 01:57:16 +0100

    remove yuka engine fixes

--- src/UniversalInjectorFramework/injector.cpp
+++ src/UniversalInjectorFramework/injector.cpp
@@ -7,7 +7,6 @@
 #include "features/allocate_console.h"
 #include "features/start_suspended.h"
 #include "features/character_substitution.h"
-#include "features/yuka_engine_fixes.h"
 #include "features/tunnel_decoder.h"
 #include "features/file_monitor.h"
 #include "features/font_manager.h"
@@ -98,7 +97,6 @@ namespace uif
 
 		initialize_feature<features::start_suspended>();
 		initialize_feature<features::character_substitution>();
-		initialize_feature<features::yuka_engine_fixes>();
 		initialize_feature<features::tunnel_decoder>();
 		initialize_feature<features::file_monitor>();
 		initialize_feature<features::font_manager>();

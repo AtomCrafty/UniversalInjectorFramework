a719875721ad30d53ce6fabbb0acde841cc73b22
AtomCrafty <atomcrafty@frucost.net>
2022-06-21 18:42:38 +0200

    add debug log utilities

--- src/UniversalInjectorFramework/utils.h
+++ src/UniversalInjectorFramework/utils.h
@@ -2,6 +2,14 @@
 
 #include <filesystem>
 
+#ifndef DEBUG_LOG
+#define DEBUG_LOG 1
+#endif
+
+#ifndef DEBUG_NOTIFY
+#define DEBUG_NOTIFY 1
+#endif
+
 namespace uif::utils {
 	bool file_exists(const wchar_t* path);
 	bool file_create_new(const wchar_t* path, const char* content, size_t length);
@@ -15,4 +23,42 @@ namespace uif::utils {
 	void* parse_address(const nlohmann::json& json);
 	void* parse_address(const std::string& string);
 	[[noreturn]] void fail(const std::string& reason);
+
+	inline void debug_notify(const char* caption, const char* message, UINT type = MB_OK | MB_ICONINFORMATION)
+	{
+#if DEBUG_NOTIFY
+		MessageBoxA(nullptr, message, caption, type);
+#endif
+	}
+
+	static void debug_log_write(HANDLE file, const char* text)
+	{
+		const DWORD length = strlen(text);
+		DWORD written = 0;
+		const auto success = WriteFile(file, text, length, &written, nullptr);
+
+		if (!success || written != length)
+			fail("Failed to write to log file");
+	}
+
+	inline void debug_log(const char* message, bool terminate = true)
+	{
+#if DEBUG_LOG
+		const auto file = CreateFileA("uif_log.log", FILE_APPEND_DATA, 0, nullptr, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+		if (file == INVALID_HANDLE_VALUE)
+			fail("Failed to open log file");
+
+		debug_log_write(file, message);
+		if (terminate) debug_log_write(file, "\n");
+		
+		CloseHandle(file);
+#endif
+	}
+
+	inline void debug_log_clear()
+	{
+#if DEBUG_LOG
+		DeleteFileA("uif_log.log");
+#endif
+	}
 }

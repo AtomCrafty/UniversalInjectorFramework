8a615bcd6bc0f222d1e8911439e1b9e321ff3a95
AtomCrafty <atomcrafty@frucost.net>
2022-06-23 02:14:46 +0200

    clean up project files

--- src/UniversalInjectorFramework/libraries.cpp
+++ src/UniversalInjectorFramework/libraries.cpp
@@ -3,10 +3,10 @@
 #include "proxies/d3d8.h"
 #include "proxies/d3d9.h"
 #include "proxies/d3d11.h"
-#include "proxies/d3dx9_43.h"
-#include "proxies/dxgi.h"
 #include "proxies/d3dcompiler_43.h"
 #include "proxies/d3dcompiler_47.h"
+#include "proxies/d3dx9_43.h"
+#include "proxies/dxgi.h"
 #include "proxies/opengl32.h"
 #include "proxies/winmm.h"
 
@@ -19,10 +19,10 @@ namespace uif::libraries
 		loaded += load_library_d3d8();
 		loaded += load_library_d3d9();
 		loaded += load_library_d3d11();
-		loaded += load_library_d3dx9_43();
-		loaded += load_library_dxgi();
 		loaded += load_library_d3dcompiler_43();
 		loaded += load_library_d3dcompiler_47();
+		loaded += load_library_d3dx9_43();
+		loaded += load_library_dxgi();
 		loaded += load_library_opengl32();
 		loaded += load_library_winmm();
 
@@ -39,10 +39,11 @@ namespace uif::libraries
 		loaded += unload_library_d3d8();
 		loaded += unload_library_d3d9();
 		loaded += unload_library_d3d11();
-		loaded += unload_library_d3dx9_43();
-		loaded += unload_library_dxgi();
 		loaded += unload_library_d3dcompiler_43();
 		loaded += unload_library_d3dcompiler_47();
+		loaded += unload_library_d3dx9_43();
+		loaded += unload_library_dxgi();
+		loaded += unload_library_opengl32();
 		loaded += unload_library_winmm();
 
 		if (!loaded)
